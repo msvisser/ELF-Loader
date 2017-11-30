e0a6acfae02f68f509d10eeb8c082e0b9b9e3fed
Michiel Visser <info@webmichiel.nl>
2017-11-30 23:36:42 +0100

    Add a recursive fibonacci function to the test code

--- test.c
+++ test.c
@@ -5,6 +5,11 @@ char other_array[16] = {0};
 
 int some_var = 123;
 
+static long long fib(long long i) {
+    if (i <= 1) return i;
+    return fib(i - 1) + fib(i - 2);
+}
+
 static void other_func() {
     printf("Hello world!\n");
     printf("some_array[0]: %d\n", some_array[0]);
@@ -18,4 +23,5 @@ void run() {
     other_func();
     printf("other_array[0]: %d\n", other_array[0]);
     printf("some_var: %d\n", some_var);
+    printf("fib(10): %lld\n", fib(10));
 }
